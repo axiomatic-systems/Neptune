df513284a56a0566c3693199fdc58be4ce4b8bff
bok <bok@2adc00ed-100a-0410-81ba-82f4cc410952>
2008-07-09 23:05:53 +0000

    Better support for MinGW

--- Source/System/Win32/NptWin32Threads.cpp
+++ Source/System/Win32/NptWin32Threads.cpp
@@ -333,7 +333,7 @@ class NPT_Win32AtomicVariable : public NPT_AtomicVariableInterface
 
  private:
     // members
-    volatile LONG m_Value;
+    volatile long m_Value;
 };
 
 /*----------------------------------------------------------------------
@@ -357,7 +357,7 @@ NPT_Win32AtomicVariable::~NPT_Win32AtomicVariable()
 int
 NPT_Win32AtomicVariable::Increment()
 {
-    return InterlockedIncrement(&m_Value);
+    return InterlockedIncrement((long*)&m_Value);
 }
 
 /*----------------------------------------------------------------------
@@ -366,7 +366,7 @@ NPT_Win32AtomicVariable::Increment()
 int
 NPT_Win32AtomicVariable::Decrement()
 {
-    return InterlockedDecrement(&m_Value);
+    return InterlockedDecrement((long*)&m_Value);
 }
 
 /*----------------------------------------------------------------------

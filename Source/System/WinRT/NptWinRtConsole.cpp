c3df1b3b864ae051d1bd73c617d12a67b29b0158
bok <bok@2adc00ed-100a-0410-81ba-82f4cc410952>
2012-05-10 18:02:56 +0000

    Added WinRT/VS2011 target

--- Source/System/WinRT/NptWinRtConsole.cpp
+++ Source/System/WinRT/NptWinRtConsole.cpp
@@ -11,10 +11,34 @@
 |   includes
 +---------------------------------------------------------------------*/
 #include "NptWinRtPch.h"
-#include <stdio.h>
 
 #include "NptConfig.h"
 #include "NptConsole.h"
+
+/*----------------------------------------------------------------------
+|   A2WHelper
++---------------------------------------------------------------------*/
+static LPWSTR A2WHelper(LPWSTR lpw, LPCSTR lpa, int nChars, UINT acp)
+{
+    int ret;
+
+    if (lpw == NULL || lpa == NULL) return NULL;
+
+    lpw[0] = '\0';
+    ret = MultiByteToWideChar(acp, 0, lpa, -1, lpw, nChars);
+    if (ret == 0) {
+        return NULL;
+    }        
+    return lpw;
+}
+
+#define NPT_WIN32_USE_CHAR_CONVERSION int _convert = 0; LPCWSTR _lpw = NULL; LPCSTR _lpa = NULL
+
+#define NPT_WIN32_A2W(lpa) (\
+    ((_lpa = lpa) == NULL) ? NULL : (\
+    _convert = (int)(strlen(_lpa)+1),\
+    (INT_MAX/2<_convert)? NULL :  \
+    A2WHelper((LPWSTR) alloca(_convert*sizeof(WCHAR)), _lpa, _convert, CP_UTF8)))
 
 /*----------------------------------------------------------------------
 |   NPT_Console::Output
@@ -22,7 +46,7 @@
 void
 NPT_Console::Output(const char* message)
 {
-    OutputDebugString(message);
-    printf("%s", message);
+	NPT_WIN32_USE_CHAR_CONVERSION;
+    OutputDebugStringW(NPT_WIN32_A2W(message));
 }
 

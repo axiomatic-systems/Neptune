a4e941dd280e2986c1c8c12476ffe59f56582656
bok <bok@2adc00ed-100a-0410-81ba-82f4cc410952>
2014-03-07 22:15:03 +0000

    Added read-only support for ZIP files (including ZIP). Only DEFLATE compression is supported Added sample virtual ZIP HTTP server

--- Source/Core/NptZip.h
+++ Source/Core/NptZip.h
@@ -37,12 +37,55 @@
 +---------------------------------------------------------------------*/
 #include "NptConfig.h"
 #include "NptStreams.h"
+#include "NptArray.h"
 
 /*----------------------------------------------------------------------
 |   class references
 +---------------------------------------------------------------------*/
 class NPT_ZipInflateState;
 class NPT_ZipDeflateState;
+
+/*----------------------------------------------------------------------
+|   NPT_ZipFile
++---------------------------------------------------------------------*/
+const unsigned int NPT_ZIP_FILE_FLAG_ENCRYPTED = 0x01;
+const unsigned int NPT_ZIP_FILE_COMPRESSION_METHOD_NONE    = 0x00;
+const unsigned int NPT_ZIP_FILE_COMPRESSION_METHOD_DEFLATE = 0x08;
+
+class NPT_ZipFile
+{
+public:
+    // types
+    class Entry {
+    public:
+        Entry(const unsigned char* data);
+        NPT_String    m_Name;
+        NPT_UInt16    m_Flags;
+        NPT_UInt16    m_CompressionMethod;
+        NPT_UInt32    m_Crc32;
+        NPT_LargeSize m_CompressedSize;
+        NPT_LargeSize m_UncompressedSize;
+        NPT_UInt16    m_DiskNumber;
+        NPT_UInt16    m_InternalFileAttributes;
+        NPT_UInt32    m_ExternalFileAttributes;
+        NPT_Position  m_RelativeOffset;
+        NPT_UInt32    m_DirectoryEntrySize;
+    };
+    
+    // class methods
+    static NPT_Result Parse(NPT_InputStream& stream, NPT_ZipFile*& file);
+    static NPT_Result GetInputStream(Entry& entry, NPT_InputStreamReference& zip_stream, NPT_InputStream*& file_stream);
+    
+    // accessors
+    NPT_Array<Entry>& GetEntries() { return m_Entries; }
+    
+private:
+    // constructor
+    NPT_ZipFile();
+    
+    // members
+    NPT_Array<Entry> m_Entries;
+};
 
 /*----------------------------------------------------------------------
 |   NPT_Zip
@@ -77,7 +120,8 @@ public:
      * Inflate (i.e decompress) a buffer
      */
     static NPT_Result Inflate(const NPT_DataBuffer& in,
-                              NPT_DataBuffer&       out);                       
+                              NPT_DataBuffer&       out,
+                              bool                  raw = false);
 };
 
 /*----------------------------------------------------------------------
@@ -86,7 +130,7 @@ public:
 class NPT_ZipInflatingInputStream : public NPT_InputStream 
 {
 public:
-    NPT_ZipInflatingInputStream(NPT_InputStreamReference& source);
+    NPT_ZipInflatingInputStream(NPT_InputStreamReference& source, bool raw = false);
    ~NPT_ZipInflatingInputStream();
    
     // NPT_InputStream methods

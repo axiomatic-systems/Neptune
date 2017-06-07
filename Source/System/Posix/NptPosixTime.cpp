6ffc1890d6e1148356d9ede8216af5ff628bb4a3
Gilles Boccon-Gibod <bok@bok.net>
2017-06-06 22:31:42 -0700

    posix local time zone

--- Source/System/Posix/NptPosixTime.cpp
+++ Source/System/Posix/NptPosixTime.cpp
@@ -28,7 +28,7 @@
 |   compatibility wrappers
 +---------------------------------------------------------------------*/
 #if defined(NPT_CONFIG_HAVE_GMTIME) && !defined(NPT_CONFIG_HAVE_GMTIME_R)
-static int gmtime_r(struct tm* _tm, time_t* time)
+static int gmtime_r(time_t* time, struct tm* _tm)
 {
     struct tm* _gmt = gmtime(time);
 
@@ -44,7 +44,7 @@ static int gmtime_r(struct tm* _tm, time_t* time)
 #endif // defined(NPT_CONFIG_HAVE_GMTIME_S
 
 #if defined(NPT_CONFIG_HAVE_LOCALTIME) && !defined(NPT_CONFIG_HAVE_LOCALTIME_R)
-static int localtime_r(struct tm* _tm, time_t* time)
+static int localtime_r(time_t* time, struct tm* _tm)
 {   
     struct tm* _local = localtime(time);
 
@@ -59,18 +59,40 @@ static int localtime_r(struct tm* _tm, time_t* time)
 }
 #endif // defined(NPT_CONFIG_HAVE_LOCALTIME_S
 
+#if defined(NPT_CONFIG_HAVE_TM_GMTOFF)
 /*----------------------------------------------------------------------
-|   NPT_DateTime::GetTimeZone
+|   NPT_DateTime::GetLocalTimeZone
 +---------------------------------------------------------------------*/
 NPT_Int32
 NPT_DateTime::GetLocalTimeZone()
 {
     struct tm tm_local;
-    time_t epoch = 0;
-    
     NPT_SetMemory(&tm_local, 0, sizeof(tm_local));
-
+    time_t epoch = 0;
     localtime_r(&epoch, &tm_local);
     
     return (NPT_Int32)(tm_local.tm_gmtoff/60);
 }
+#else
+/*----------------------------------------------------------------------
+|   NPT_DateTime::GetLocalTimeZone
++---------------------------------------------------------------------*/
+NPT_Int32
+NPT_DateTime::GetLocalTimeZone()
+{
+    time_t epoch = 0;
+    
+    struct tm tm_gmt;
+    NPT_SetMemory(&tm_gmt, 0, sizeof(tm_gmt));
+    gmtime_r(&epoch, &tm_gmt);
+
+    struct tm tm_local;
+    NPT_SetMemory(&tm_local, 0, sizeof(tm_local));
+    localtime_r(&epoch, &tm_local);
+    
+    time_t time_gmt   = mktime(&tm_gmt);
+    time_t time_local = mktime(&tm_local);
+    
+    return (NPT_Int32)((time_local-time_gmt)/60);
+}
+#endif

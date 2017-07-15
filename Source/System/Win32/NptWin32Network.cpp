54a95a48b46465f245f2ab05b0f703ce878ef368
Gilles Boccon-Gibod <bok@bok.net>
2017-07-15 12:46:27 -0700

    added vs-2015 target

--- Source/System/Win32/NptWin32Network.cpp
+++ Source/System/Win32/NptWin32Network.cpp
@@ -11,6 +11,7 @@
 |   includes
 +---------------------------------------------------------------------*/
 #define STRICT
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include "NptNetwork.h"
